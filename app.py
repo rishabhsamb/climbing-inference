4146e062a7159aafd2ebaaeb8e39338c4960504b
rishabh sambare <rishabhsamb@gmail.com>
2021-12-31 17:11:47 -0500

    cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import pickle
 from numpy import array
 import os
@@ -7,8 +8,12 @@ file = open('./dumped-pipeline.pkl', 'rb')
 clf = pickle.load(file)
 file.close()
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/predict")
+@cross_origin()
 def predict():
     try:
         difference = request.json['difference']
